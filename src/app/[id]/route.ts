62a511c38ee2af31edfd4cec1f32c0fcf6500924
KreakxX <Henrik.standke@web.de>
2025-07-19 12:01:00 +0200

    App Done

--- src/app/[id]/route.ts
+++ src/app/[id]/route.ts
@@ -4,20 +4,24 @@ import { NextRequest } from "next/server";
 
 const prisma = new PrismaClient()
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-  const { id } = params;
-    try {
-        const link = await prisma.link.findUnique({
-            where: { Short_Link: id },
-        });
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
 
-        if (!link) {
-            return NextResponse.json({ error: 'Link not found' }, { status: 404 });
-        }
+  try {
+    const link = await prisma.link.findUnique({
+      where: { Short_Link: id },
+    });
 
-        return NextResponse.redirect(link.ref_link);
-    } catch (error) {
-        console.error('Error fetching link:', error);
-        return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+    if (!link) {
+      return NextResponse.json({ error: 'Link not found' }, { status: 404 });
     }
+
+    return NextResponse.redirect(link.ref_link);
+  } catch (error) {
+    console.error('Error fetching link:', error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+  }
 }
